00c7bd3899f87d5293626fe74a2ed1e8a0dda54e
Manyam-sri-hari-priya <76106636+Manyam-sri-hari-priya@users.noreply.github.com>
2023-04-12 21:14:56 +0530

    code update

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer-center p-3 shadow-lg bg-slate-200 text-black">
+      <div className="text-center">
+        <p>
+          Copyright © 2023 - Anupriya, Manyam Sri Hari Priya, Priyadarshini &nbsp;
+          <a
+            className="font-semibold"
+            href="mailto:jaimin.chokhawala@gmail.com"
+          >
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
